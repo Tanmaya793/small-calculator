84dd05ae9d0ffc713e108b20c0e33298b22b4c0c
Tanmaya <tanmayanaik2006@gmail.com>
2025-06-16 21:50:05 +0530

    tracking-01

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/calculator/<int:num1>/<int:num2>")
+def calculator(num1,num2):
+    return render_template("calc.html", n1 = num1, n2 = num2)
+
+@app.route("/add/<int:num1>/<int:num2>")
+def add(num1, num2):
+    return f"<h1>{num1} + {num2} = {num1 + num2}<h2>"
+
+@app.route("/sub/<int:num1>/<int:num2>")
+def sub(num1, num2):
+    return f"<h1>{num1} - {num2} = {num1 - num2}<h2>"
+
+@app.route("/mul/<int:num1>/<int:num2>")
+def mul(num1, num2):
+    return f"<h1>{num1} * {num2} = {num1 * num2}<h2>"
+
+@app.route("/div/<int:num1>/<int:num2>")
+def div(num1, num2):
+    return f"<h1>{num1} / {num2} = {num1 / num2}<h2>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
